3b019dbd301d80bed11c817c1d16fe44d456d45c
Neravin <neravind@gmail.com>
2014-07-15 17:01:10 +0400

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715071222) do
+ActiveRecord::Schema.define(version: 20140715122847) do
+
+  create_table "links", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "site"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "title"
