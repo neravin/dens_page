3894b978fe3d2cff4cae7957aeb09ec4001aae28
Neravin <neravind@gmail.com>
2014-07-20 18:08:28 +0400

    resolution of the same name

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,5 @@ class Service < ActiveRecord::Base
   has_and_belongs_to_many :projects
 
   validates :name, :price, presence: true
-  validates :name, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
