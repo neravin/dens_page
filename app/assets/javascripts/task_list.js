443043c26c4c378bce83ef975f63f3c3e60e8a5d
Neravin <neravind@gmail.com>
2014-08-07 12:15:31 +0400

    add color for sticky

--- app/assets/javascripts/task_list.js
+++ app/assets/javascripts/task_list.js
@@ -8,36 +8,49 @@ function init() {
 
   for( var i = 0; i < stickiesArray.length; i++ ){
     var key   = stickiesArray[i];
-    var value = localStorage[key];
+    var value = JSON.parse(localStorage[key]);
 
     addStickyToDOM(key, value);
   }
 }
 
 function createSticky() {
-  var stickiesArray = getStickiesArray();
-  var currentData   = new Date();
-  var key           = "sticky_" + currentData.getTime();
-  var value         = document.getElementById("note_text").value;
+  var stickiesArray  = getStickiesArray();
+  var currentData    = new Date();
+  var colorSelectObj = document.getElementById("note_color");
+  var index          = colorSelectObj.selectedIndex;
+  var color          = colorSelectObj[index].value;
+  var key            = "sticky_" + currentData.getTime();
+  var value          = document.getElementById("note_text").value;
+  var stickyObj      = {
+    "value": value,
+    "color": color
+  };
 
-  localStorage.setItem(key, value);
+  localStorage.setItem(key, JSON.stringify(stickyObj));
   stickiesArray.push(key);
   localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
-  addStickyToDOM(key, value);
+  addStickyToDOM(key, stickyObj);
 }
 
 
-function addStickyToDOM(key, value) {
+function addStickyToDOM(key, stickyObj) {
   var stickies = document.getElementById("stickies");
   var sticky   = document.createElement("li");
   var span     = document.createElement("span");
+  var delete_sticky = document.createElement("span");
 
   sticky.setAttribute("id", key);
+  sticky.style.backgroundColor = stickyObj.color;
   span.setAttribute("class", "sticky");
-  span.innerHTML = value;
+  span.innerHTML = stickyObj.value;
+  delete_sticky.setAttribute("id", "delete-sticky");
+  delete_sticky.setAttribute("class", "fa fa-times")
+
   sticky.appendChild(span);
+  sticky.appendChild(delete_sticky);
   stickies.appendChild(sticky);
-  sticky.onclick = deleteSticky;
+  delete_sticky.onclick = deleteSticky;
 }
 
 function getStickiesArray(){
@@ -56,11 +69,13 @@ function getStickiesArray(){
 
 function deleteSticky(e){
   var stickiesArray = getStickiesArray();
-  var key           = e.target.id;
+  //var key           = e.target.id;
 
-  if( e.target.tagName.toLowerCase() == "span" ){
+  /*if( e.target.tagName.toLowerCase() == "span" ){
     key = e.target.parentNode.id;
-  }
+  }*/
+
+  key = e.target.parentNode.id;
 
   localStorage.removeItem(key);
   if( stickiesArray ) {
